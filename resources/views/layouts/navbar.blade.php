87123428ea5720354eee1577e29a75c28c183ecb
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-04-05 15:52:54 -0500

    Adding layout

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,7 @@
+<!-- Stored in resources/views/layouts/navbar.blade.php -->
+<nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1">PlaceToPay</span>
+  <span class="navbar-text">
+    @paleox
+  </span>
+</nav>
